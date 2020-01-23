c5c5c81f1278660c99552b69cb0e51c2ee17354c
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2020-01-23 11:58:58 +0100

    Add .gitignore and mongoose connection

--- app.js
+++ app.js
@@ -2,35 +2,46 @@ const express = require('express');
 // Morgan logs incoming requests
 const morgan = require('morgan');
 // Allows to parse request body
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const mealsRoutes = require('./api/routes/meals');
 
+mongoose.connect(
+  'mongodb+srv://admin:' +
+    process.env.MONGO_DB_PASSWORD +
+    '@meals-44tke.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
+console.log(process.env.MONGO_DB_PASSWOR);
+
 // Set request logs
 app.use(morgan('dev'));
 
 // Set accepted body formats
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Set CORS configuration
 app.use((req, res, next) => {
   // Allow anyone to make request
-  req.header("Access-Control-Allow-Origin", "*");
+  req.header('Access-Control-Allow-Origin', '*');
 
   // Set which information can be included into the header
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
   // Browser will send OPTIONS before PUT or PATCH request to check possibility of making request
-  if(req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE")
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
     return res.status(200).json({});
   }
   next();
 });
-
 
 // Set handlers for specific routes
 app.use('/meals', mealsRoutes);
@@ -43,12 +54,12 @@ app.use((req, res, next) => {
 });
 
 // Handle any error
-app.use((error, req,res,next) => {
+app.use((error, req, res, next) => {
   // error 500 - Internal server error
   res.status(error.status || 500);
   res.json({
     message: error.message
-  })
+  });
 });
 
 module.exports = app;
