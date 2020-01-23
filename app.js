a507eacd6c8e1761b6aafdbc04c4865eb5854a84
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2020-01-23 10:58:44 +0100

    Add CORS configuration

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ const app = express();
 
 const mealsRoutes = require('./api/routes/meals');
 
+// Set request logs
 app.use(morgan('dev'));
 
 // Set accepted body formats
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Set CORS configuration
+app.use((req, res, next) => {
+  // Allow anyone to make request
+  req.header("Access-Control-Allow-Origin", "*");
+
+  // Set which information can be included into the header
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  
+  // Browser will send OPTIONS before PUT or PATCH request to check possibility of making request
+  if(req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE")
+    return res.status(200).json({});
+  }
+  next();
+});
+
 
 // Set handlers for specific routes
 app.use('/meals', mealsRoutes);
